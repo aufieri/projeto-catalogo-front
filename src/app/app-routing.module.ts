fae3d4f0e2587b8f5ee7c5981b66e36d0d98f40c
aufieri <aufieriz@gmail.com>
2023-11-05 23:28:27 -0300

    tabela de tabela produto

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NavComponent } from './components/nav/nav.component';
+import { HomeComponent } from './components/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', component: NavComponent, children: [
+      
+      {  path: 'home', component: HomeComponent}
+      
+    ]
+  },
+
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
